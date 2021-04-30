d263ad82bdfb5f44501dceb0df3e6a5c89feffae
JieHuiKoo <31171083+JieHuiKoo@users.noreply.github.com>
2021-04-30 11:19:29 +0800

    Added error catching for Data and Output folder

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from PyPDF2 import PdfFileReader as pdfFR, PdfFileWriter as pdfFW, pdf, PdfFileMerger as pdfFM # For dealing with pdf
 import os # For dealing with file directory
+import sys
 
-def obtain_pdfs():
-    # Obtain current directory main.py program is in
-    current_directory = os.getcwd()
+def obtain_pdfs(current_directory):
+    # Find the data folder
+    data_folder_directory = ''
+    for directory in os.walk(current_directory):
+        if(directory[0].split("\\")[-1]).lower() == 'data':
+            data_folder_directory = directory[0]
+            break
+    if data_folder_directory == '':
+        sys.exit('Error: Data folder not found!')
 
-    # location of all exam papers
-    data_folder_directory = current_directory + "\Data"
-    
     # Examine the folder and make a data structure
     data_folder = os.walk(data_folder_directory)
 
@@ -18,7 +22,6 @@ def obtain_pdfs():
     for year_folder in data_folder: # year_folder contains = [directory, list of subfolders, list of file names in year_folder]
         
         # === Obtain the year ===
-
         # Obtain folder directory
         folder_directory = year_folder[0]
         # Split by space
@@ -56,21 +59,16 @@ def obtain_pdfs():
     # Sort our data structure by file name => [file directory, year, file name, original exam paper name]
     exam_papers_collated.sort(key = lambda exam_papers_collated: exam_papers_collated[1], reverse=True)
     return exam_papers_collated
-def export_pdf(data):
-    # Obtain the current directory of where the main.py is stored
-    current_directory = os.getcwd()
-
-    # Generate the output directory
-    output_directory = current_directory + str("\output")
-
+def export_pdf(data, current_directory, output_directory):
     # If output directory does not exist, create it
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
+    else:
+        sys.exit('Error: Please delete the output folder first!')
 
     progression_count = 1
-    
     for exam_paper in data:
-        print("Processing " + str(progression_count) + " of " + str(len(data)+1) + " || Year: " + exam_paper[1] + " || " + exam_paper[2])
+        print("Processing " + str(progression_count) + " of " + str(len(data)) + " || Year: " + exam_paper[1] + " || " + exam_paper[2])
         progression_count = progression_count + 1
         try:
             #print("Opening Existing")
@@ -79,9 +77,6 @@ def export_pdf(data):
 
             # Open the file to be merged
             exam_file_to_merge = pdfFR(open(exam_paper[0], 'rb'))
-
-            # Get number of pages
-            num_of_pages = exam_file_to_merge.getNumPages()
 
             merger = pdfFM()
             # Merge it
@@ -109,10 +104,14 @@ def export_pdf(data):
         merger.write(output_directory + '\\' + str(exam_paper[2]))
         merger.close()
 
+# Obtain the current directory of where the main.py is stored
+current_directory = os.getcwd()
+
+# Generate the output directory
+output_directory = current_directory + str("\output")
+
 # Obtain the data structure of exam papers
-data = obtain_pdfs()
+data = obtain_pdfs(current_directory)
 
 # Merge and export the pdfs
-export_pdf(data)
-
-print("Bubba <3 Wubba")
+export_pdf(data, current_directory, output_directory)
